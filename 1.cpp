c233ce9ed47e4180ab688ce5eb459965879094fd
jason <1951016565@qq.com>
2018-08-13 10:30:07 +0800

    cc

---
+++ 1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+bool vis[10];
+memset(vis, 0, sizeof(vis));
+
+
+int main()
+{
+	freopen("div.cpp.in", "r",stdin);
+	freopen("div.cpp.out","w",stdout); 
+		memset(vis,0,sizeof(vis));
+	int i, k, n , h , g;
+	int flag = 0;
+	int a[5],b[5];
+	cin>>n;
+	/*if(n>72)
+	{
+		return 0;
+	}
+	
+	for (i = 1000; i <= 98765; i++)
+	{
+		int j = i * n;
+		if(judge(i)&&judge)
+
+				flag = 1;
+		
+		
+	}
+	if(flag==0)
+	printf("No Answer/n");*/
+	printf("NO Answer/n");
+	return 0;
+} 
